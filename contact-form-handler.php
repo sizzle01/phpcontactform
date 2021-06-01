13461a43b8b79a8bdd00de9c90f580ffe259e0b8
Your Name <you@example.com>
2021-06-01 13:20:43 +0100

    initial commit

---
+++ contact-form-handler.php
@@ -0,0 +1,21 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+
+$email_from = 'talktoaliyu@gmail.com';
+$email_subject= 'New Form Submission';
+
+$email_body = "user name: $name.\n".
+             "user Email: $email.\n".
+             "user phone: $phone.\n".
+             "User Message: $message.\n";
+
+$to = "deloyemoranigltd@gmail.com"; 
+
+$headers = "From: $email_from \r\n";
+$headers .= "Reply-To: $email \r\n";
+ mail($to, $email_subject, $email_body, $headers);
+ header("Location: index.html");
+?>   
